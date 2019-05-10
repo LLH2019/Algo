f00a70fbc89e969901cabb8777e98510542f29dc
LLH2019 <46324430+LLH2019@users.noreply.github.com>
2019-05-10 18:26:19 +0800

    add some explanation
    为代码增加一些说明

--- Tree/searchRoute.cpp
+++ Tree/searchRoute.cpp
@@ -1,11 +1,20 @@
+/*
+    首先初始化条件 
+    一个空栈、一个二叉树根节点、给定需要搜索值
+    整个解题思路：  先遍历左子树、判断该路径上有无给定节点值，接着进行出栈处理，接着再遍历右子树
+    注意点： 栈中保存的节点一定不为null，在对哪些节点需要出栈时，其中都是取栈中元素
+*/
 void searchRoute(BiTNode* bt, TElemType data) {
     stack<BiTree> s;
+    // 当栈不为空且遍历节点不为null时才继续循环
     while(bt != nullptr || !s.empty()) {
+        // 遍历左子树
         while(bt != nullptr && bt->data != data) {
             s.push(bt);
             bt->tag = 0;
             bt = bt->lchild;
         }
+        // 如果找到节点
         if(bt->data == data) {
             // 出栈得到路径
             while(!s.empty()) {
@@ -14,9 +23,12 @@ void searchRoute(BiTNode* bt, TElemType data) {
                 s.pop();
             }
         }
+        // 对栈中的节点进行出栈处理， 注意是栈中节点
         while(!s.empty() && s.top()->tag==1) {
             s.pop();
         }
+        // 将栈顶元素的右节点赋值给bt
+        // 并且将栈顶元素节点设置为已访问过
         if(!s.empty()) {
             s.top()->tag = 1;
             bt = s.top()->rchild;
